0b1d523a615111790ba9d46836e669a5c05beeb2
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-26 03:21:35 +0600

    made profile dynamic

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import useAuth from '../../hooks/useAuth';
 import useAxios from '../../hooks/useAxios';
 import Loading from '../../pages/Loading/Loading';
@@ -11,18 +12,17 @@ const Profile = () => {
     const axios = useAxios();
 
     const [profileData, setProfileData] = useState({
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        phone: '123-456-7890',
+        name: '',
+        phone: '',
         interests: "",
       });
   
 
       const getUserInfo = async () => {
-        const result = await axios.get(`/api/user/get-user-info?email=${user?.email}`);
-        // console.log(result.data.result)
-        const {title, image, category, shortDesc, longDesc} = result.data.result;
-          setBlogData({title, image, category, shortDesc, longDesc});
+        const result = await axios.post(`/api/user/get-user-info?email=${user?.email}`);
+        // console.log(result.data.data.name)
+        const {name, phone, interests} = result?.data?.data;
+        setProfileData({name, phone, interests})
         return result;
       }
 
@@ -41,33 +41,42 @@ if(isLoading)
     return <Loading />
 }
   
-console.log(data)
+// console.log(data.data.data.name)
 
-  const [isEditing, setIsEditing] = useState(false);
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setProfileData((prevData) => ({ ...prevData, [name]: value }));
+    setProfileData({ ...profileData, [name]: value });
   };
 
  
+  const handleUpdate = async (e)=>{
+    e.preventDefault();
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
+    const {name, phone, interests} = profileData
+    const obj = {
+        email: user.email,
+        name,
+        phone,
+        interests
+    }
 
-  const handleSaveClick = () => {
+    const res = await axios.put('/api/user/update-user-info', obj);
+// console.log(res.data.message)
+    toast.dismiss();
+    toast.success(res.data.message)
 
-    setIsEditing(false);
+  }
 
-  };
+
 
   return (
     <div className="container mx-auto mt-8 px-20">
       <div className="max-w-full mx-auto p-4 border shadow-2xl">
         <div className="bg-white p-4 rounded-lg">
           <h1 className="text-3xl font-semibold mb-4">My Profile</h1>
-          <form className='my-5'>
+          <form className='my-5' onSubmit={handleUpdate}>
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="name">
                 Name
@@ -80,6 +89,7 @@ console.log(data)
                 onChange={handleInputChange}
                 className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
                 placeholder='Your name'
+                required
               />
             </div>
             <div className="mb-4">
@@ -105,6 +115,7 @@ console.log(data)
                 onChange={handleInputChange}
                 className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
                 placeholder='Your phone number'
+                required
               />
             </div>
 
@@ -120,10 +131,11 @@ console.log(data)
                 onChange={handleInputChange}
                 className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
                 placeholder='Interests'
+                required
               />
             </div>
             <div className='text-center pt-5'>
-                <button className='px-3 text-lg font-bold text-white bg-blue-600 py-1 rounded w-44'>Save</button>
+                <button type='submit' className='px-3 text-lg font-bold text-white bg-blue-600 py-1 rounded w-44'>Save</button>
             </div>
           </form>
           
