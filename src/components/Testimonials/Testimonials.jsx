a02f1426816e44485cc9e5d5cc6064fedf3ca1c4
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-29 03:48:06 +0600

    testmonials problem fixed

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -16,8 +16,7 @@ const Testimonials = () => {
     queryFn: getFeedbacks
   })
 
-  if(isLoading)
-  {
+  if (isLoading) {
     <Loading />
   }
 
@@ -27,76 +26,87 @@ const Testimonials = () => {
 
     <div>
       {
-        data?.data?.data?.length > 2 &&
+        data?.data?.data?.length > 0 &&
         <div className='px-10 my-10'>
           <div>
             <h2 className="text-3xl font-bold mt-12 mb-3 text-center">Testimonials</h2>
           </div>
           <div className="carousel w-full h-80 border shadow-2xl">
-            <div id="slide1" className="carousel-item relative w-full">
-              <div className="w-full flex flex-col justify-center items-center">
-                <p className='font-bold text-2xl'>{data?.data.data[0].participant}</p>
-                <p className='text-xs font-bold text-gray-600'>{data?.data?.data[0]?.campId.name}</p>
-                {/* <p className='text-xl font-bold text-gray-600'><span className=' text-orange-500 px-1'>★</span></p> */}
-                {data?.data.data[0].rating === 1 && <p className='text-2xl font-bold text-orange-500'>★</p>}
-                {data?.data.data[0].rating === 2 && <p className='text-2xl font-bold text-orange-500'>★ ★</p>}
-                {data?.data.data[0].rating === 3 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★</p>}
-                {data?.data.data[0].rating === 4 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★</p>}
-                {data?.data.data[0].rating === 5 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★ ★</p>}
-
-
-                <p className='w-2/4 mt-5 text-center text-gray-500 font-bold'>❝ {data?.data.data[0].feedback} ❞</p>
-
-              </div>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide3" className="btn btn-circle">❮</a>
-                <a href="#slide2" className="btn btn-circle">❯</a>
-              </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-              <div className="w-full flex flex-col justify-center items-center">
-                <p className='font-bold text-2xl'>{data?.data.data[1].participant}</p>
-                <p className='text-xs font-bold text-gray-600'>{data?.data.data[1].campId.name}</p>
-                {/* <p className='text-xl font-bold text-gray-600'><span className=' text-orange-500 px-1'>★</span></p> */}
-                {data?.data.data[1].rating === 1 && <p className='text-2xl font-bold text-orange-500'>★</p>}
-                {data?.data.data[1].rating === 2 && <p className='text-2xl font-bold text-orange-500'>★ ★</p>}
-                {data?.data.data[1].rating === 3 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★</p>}
-                {data?.data.data[1].rating === 4 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★</p>}
-                {data?.data.data[1].rating === 5 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★ ★</p>}
-
-
-                <p className='w-2/4 mt-5 text-center text-gray-500 font-bold'>❝ {data?.data.data[1].feedback} ❞</p>
-
-              </div>
-
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn btn-circle">❮</a>
-                <a href="#slide3" className="btn btn-circle">❯</a>
-              </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-              <div className="w-full flex flex-col justify-center items-center">
+            {
+              data?.data?.data[1] &&
+              <div id="slide1" className="carousel-item relative w-full">
                 <div className="w-full flex flex-col justify-center items-center">
-                  <p className='font-bold text-2xl'>{data?.data.data[2].participant}</p>
-                  <p className='text-xs font-bold text-gray-600'>{data?.data?.data[1].campId.name}</p>
-                  {/* <p className='text-xl font-bold text-gray-600'><span className=' text-orange-500 px-1'>★</span></p> */}
-                  {data?.data.data[2].rating === 1 && <p className='text-2xl font-bold text-orange-500'>★</p>}
-                  {data?.data.data[2].rating === 2 && <p className='text-2xl font-bold text-orange-500'>★ ★</p>}
-                  {data?.data.data[2].rating === 3 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★</p>}
-                  {data?.data.data[2].rating === 4 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★</p>}
-                  {data?.data.data[2].rating === 5 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★ ★</p>}
+                  <p className='font-bold text-2xl'>{data?.data.data[0].participant}</p>
+                  <p className='text-xs font-bold text-gray-600'>{data?.data?.data[0]?.campId.name}</p>
+
+                  {data?.data.data[0].rating === 1 && <p className='text-2xl font-bold text-orange-500'>★</p>}
+                  {data?.data.data[0].rating === 2 && <p className='text-2xl font-bold text-orange-500'>★ ★</p>}
+                  {data?.data.data[0].rating === 3 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★</p>}
+                  {data?.data.data[0].rating === 4 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★</p>}
+                  {data?.data.data[0].rating === 5 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★ ★</p>}
 
 
-                  <p className='w-2/4 mt-5 text-center text-gray-500 font-bold'>❝ {data?.data.data[2].feedback} ❞</p>
+                  <p className='w-2/4 mt-5 text-center text-gray-500 font-bold'>❝ {data?.data.data[0].feedback} ❞</p>
+
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide3" className="btn btn-circle">❮</a>
+                  <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+              </div>
+            }
+
+            {
+              data?.data?.data[1] &&
+              <div id="slide2" className="carousel-item relative w-full">
+                <div className="w-full flex flex-col justify-center items-center">
+                  <p className='font-bold text-2xl'>{data?.data.data[1].participant}</p>
+                  <p className='text-xs font-bold text-gray-600'>{data?.data.data[1].campId.name}</p>
+
+                  {data?.data.data[1].rating === 1 && <p className='text-2xl font-bold text-orange-500'>★</p>}
+                  {data?.data.data[1].rating === 2 && <p className='text-2xl font-bold text-orange-500'>★ ★</p>}
+                  {data?.data.data[1].rating === 3 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★</p>}
+                  {data?.data.data[1].rating === 4 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★</p>}
+                  {data?.data.data[1].rating === 5 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★ ★</p>}
+
+
+                  <p className='w-2/4 mt-5 text-center text-gray-500 font-bold'>❝ {data?.data.data[1].feedback} ❞</p>
 
                 </div>
 
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide1" className="btn btn-circle">❮</a>
+                  <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
               </div>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn btn-circle">❮</a>
-                <a href="#slide1" className="btn btn-circle">❯</a>
+            }
+
+            {
+              data?.data?.data[2] &&
+              <div id="slide3" className="carousel-item relative w-full">
+                <div className="w-full flex flex-col justify-center items-center">
+                  <div className="w-full flex flex-col justify-center items-center">
+                    <p className='font-bold text-2xl'>{data?.data.data[2].participant}</p>
+                    <p className='text-xs font-bold text-gray-600'>{data?.data?.data[1].campId.name}</p>
+
+                    {data?.data.data[2].rating === 1 && <p className='text-2xl font-bold text-orange-500'>★</p>}
+                    {data?.data.data[2].rating === 2 && <p className='text-2xl font-bold text-orange-500'>★ ★</p>}
+                    {data?.data.data[2].rating === 3 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★</p>}
+                    {data?.data.data[2].rating === 4 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★</p>}
+                    {data?.data.data[2].rating === 5 && <p className='text-2xl font-bold text-orange-500'>★ ★ ★ ★ ★</p>}
+
+
+                    <p className='w-2/4 mt-5 text-center text-gray-500 font-bold'>❝ {data?.data.data[2].feedback} ❞</p>
+
+                  </div>
+
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide2" className="btn btn-circle">❮</a>
+                  <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
               </div>
-            </div>
+            }
 
           </div>
         </div>
