a441abcdad3e494c5042b3a7fcf4da543833fcb4
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-30 19:09:15 +0600

    added pagination on available camp page and updated readme file

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# React + Vite
+# Aid Camp
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## [ Live Site Link]()
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- **Secure Access:**
+  - Role-based authentication ensures personalized dashboards for participants, professionals, and organizers.
+
+- **Simple Registration:**
+  - Participants effortlessly register for health camps, streamlining event enrollment.
+
+- **Convenient Payments:**
+  - Integrated payment system allows online transactions, with a transparent payment history feature.
+
+- **Professional Interaction:**
+  - Professionals express interest, monitor request status, fostering collaboration.
+
+- **Organizer Control:**
+  - Organizers can create camps, manage camps, oversee requests, and approve/cancel efficiently for a responsive system.
--- src/components/CampsAvailable/CampsAvailable.jsx
+++ src/components/CampsAvailable/CampsAvailable.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import useAxios from '../../hooks/useAxios';
 import Loading from '../../pages/Loading/Loading';
 import CampCard from '../CampCard/CampCard';
@@ -8,38 +8,83 @@ const CampsAvailable = () => {
 
     const axios = useAxios();
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
     const getAvailableCamps = async () => {
-      const res = await axios.get(`/api/camp/get-available-camps`);
-      return res;
+        const res = await axios.get(`/api/camp/get-available-camps?page=${currentPage}`);
+
+        setTotalPages(res.data.pagination.totalPages)
+        return res;
     }
 
 
-const {data, isLoading} = useQuery({
- queryKey: ["availableCamps"],
- queryFn: getAvailableCamps
-})
+    const { data, isLoading } = useQuery({
+        queryKey: ["availableCamps", currentPage],
+        queryFn: getAvailableCamps
+    })
 
-if(isLoading)
-{
-    return <Loading />
-}
+    if (isLoading) {
+        return <Loading />
+    }
 
-// console.log(data.data.data)
+    console.log(data.data.data)
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
 
 
     return (
         <div>
             <div className='text-2xl pt-28 text-center font-bold text-gray-600'><h1>Available Camps</h1></div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-10 py-10'>
-                
+
                 {
-                    data?.data?.data.map((element)=>(
+                    data?.data?.data.map((element) => (
                         <CampCard key={element._id} element={element} />
                     ))
                 }
 
 
             </div>
+
+
+            {/* pagination */}
+
+            <div className="flex items-center justify-center pb-5">
+                {currentPage > 1 && (
+                    <button
+                        onClick={() => handlePageChange(currentPage - 1)}
+                        className="mx-1 px-3 py-1 rounded bg-gray-200 text-gray-800"
+                    >
+                        {"< Prev"}
+                    </button>
+                )}
+
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                        key={index + 1}
+                        onClick={() => handlePageChange(index + 1)}
+                        className={`mx-1 px-3 py-1 rounded ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
+                            }`}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+
+                {currentPage < totalPages && (
+                    <button
+                        onClick={() => handlePageChange(currentPage + 1)}
+                        className="mx-1 px-3 py-1 rounded bg-gray-200 text-gray-800"
+                    >
+                        {"Next >"}
+                    </button>
+                )}
+            </div>
+
+
+
         </div>
     );
 };
